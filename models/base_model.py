621792dd02337e2c915930cc5477a4fda686a81a
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-06 23:52:11 +0000

    updated variable name and serialization check

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,43 @@
 #!/usr/bin/python3
 """
-Models module
+Models File with BaseModel Class
 """
 import uuid
+import json
 from datetime import datetime
 
 
 class BaseModel:
     def __init__(self):
+        """instantiation of new BaseModel Class"""
         self.id = uuid.uuid1()
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def save(self):
+        """updates attribute updated_at to current time"""
         self.updated_at = datetime.now()
+
+    def is_serializable(self, obj_x):
+        """checks if object is serializable"""
+        try:
+            json.dumps(obj_x)
+            return True
+        except:
+            return False
 
     def to_json(self):
         """returns json representation of self"""
-        D = dict()
+        bm_dict = {}
         for k, v in (self.__dict__).items():
-            if (isinstance(v, (datetime, uuid.UUID))):
-                D[k] = str(v)
+            if (self.is_serializable(v)):
+                bm_dict[k] = v
             else:
-                D[k] = v
-        D["__class__"] = self.__class__.__name__
-        return(D)
+                bm_dict[k] = str(v)
+        bm_dict["__class__"] = self.__class__.__name__
+        return(bm_dict)
 
     def __str__(self):
+        """returns string type representation of object instance"""
         return "[{:}] ({:}) {:}".format(type(self).__name__, self.id,
-                                          self.__dict__)
-
-if __name__ == '__main__':
-    BaseModel
+                                        self.__dict__)
