237272692febb2a7cba8efae23d2b96b8b48abed
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-10 17:43:19 +0000

    updated __init__ with more key value pairs for CLS

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,21 @@
 from models.engine import file_storage
-from models.user import User
 from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
-from models.state import State
 from models.review import Review
+from models.state import State
+from models.user import User
 
 storage = file_storage.FileStorage()
 storage.reload()
 
 CLS = {
     'BaseModel': BaseModel,
+    'Amenity': Amenity,
+    'City': City,
+    'Place': Place,
+    'Review': Review,
+    'State': State,
     'User': User
     }
