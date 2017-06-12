17671de7f06c0af90947371c777a39bdc55d36f6
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-12 22:39:44 +0000

    updated file storage to use it's own attribute instead of importing from models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,15 +3,30 @@
 Handles I/O, writing and reading, of JSON for storage of all class instances
 """
 import json
-import models
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from datetime import datetime
 
+strptime = datetime.strptime
 to_json = BaseModel.to_json
 
 
 class FileStorage:
     """handles long term storage of all class instances"""
-
+    CLS = {
+        'BaseModel': BaseModel,
+        'Amenity': Amenity,
+        'City': City,
+        'Place': Place,
+        'Review': Review,
+        'State': State,
+        'User': User
+    }
     __file_path = './dev/file.json'
     __objects = {}
 
@@ -36,18 +51,19 @@ class FileStorage:
     def reload(self):
         """if file exists, deserializes JSON file to __objects, else nothing"""
         fname = FileStorage.__file_path
+        FileStorage.__objects = {}
         try:
             with open(fname, mode='r', encoding='utf-8') as f_io:
                 new_objs = json.load(f_io)
-            for o_id, d in new_objs.items():
-                k_cls = d['__class__']
-                if not isinstance(d['created_at'], datetime):
-                    d['created_at'] = strptime(d['created_at'],
-                                               "%Y-%m-%d %H:%M:%S.%f")
-                if 'updated_at' in d:
-                    if not isinstance(d['updated_at'], datetime):
-                        d['updated_at'] = strptime(d['updated_at'],
-                                                   "%Y-%m-%d %H:%M:%S.%f")
-                FileStorage.__objects[o_id] = CLS[k_cls](**o_dict)
         except:
-            pass
+            return
+        for o_id, d in new_objs.items():
+            k_cls = d['__class__']
+            if not isinstance(d['created_at'], datetime):
+                d['created_at'] = strptime(d['created_at'],
+                                           "%Y-%m-%d %H:%M:%S.%f")
+            if 'updated_at' in d:
+                if not isinstance(d['updated_at'], datetime):
+                    d['updated_at'] = strptime(d['updated_at'],
+                                               "%Y-%m-%d %H:%M:%S.%f")
+            FileStorage.__objects[o_id] = FileStorage.CLS[k_cls](**d)
