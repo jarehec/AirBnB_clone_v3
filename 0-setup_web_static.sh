ac0bb13c131e1bb9e3153138a9b28eaf09774c1d
Kimberly Wong <kjowong@gmail.com>
2017-08-20 18:49:39 +0000

    Adding backslash

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,8 +2,8 @@
 # Bash scrip to set up web servers to deploy web_static
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo mkdir -p data/web_static/shared/
-sudo mkdir -p data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 sudo echo "<html>
   <head>
   </head>
@@ -12,6 +12,6 @@ sudo echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu data
+sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '53i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
-
+sudo service nginx restart
