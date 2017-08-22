2b768ece25676467f8005e93775b64a9870efc10
Kimberly Wong <kjowong@gmail.com>
2017-08-22 00:40:23 +0000

    adding fabric script to compress files into tar

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Fabric script that generates tgz archive from contents of web_static
+"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+        generates a .tgz archine from contents of web_static
+    """
+    time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    file_name = "versions/web_static_{}.tgz".format(time)
+    archive = ''
+    archive += local("mkdir -p ./versions")
+    archive += local("tar --create --verbose -z --file={} ./web_static"
+                     .format(file_name))
+    if archive is None:
+        return None
+    else:
+        return archive
