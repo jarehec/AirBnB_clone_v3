8d49eb0581e87fc12f5ff3184ef832bfe36fd8c5
Kimberly Wong <kjowong@gmail.com>
2017-08-22 08:16:39 +0000

    Importing importlib

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,6 +7,7 @@ from fabric.api import *
 from fabric.operations import run, put, sudo, local
 from datetime import datetime
 import os
+from importlib import import_module
 pack_web = import_module('1-pack_web_static')
 deploy_web = import_module('2-do_deploy_web_static')
 env.hosts = ['66.70.184.249', '54.210.138.75']
