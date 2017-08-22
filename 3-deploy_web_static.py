c74771e5adb990a9e332e0e55f08edde3df2a23c
Kimberly Wong <kjowong@gmail.com>
2017-08-22 05:06:48 +0000

    Adding executable

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@
 from fabric.api import *
 pack_web = __import__('1-pack_web_static')
 deploy_web = __import__('2-do_deploy_web_static')
-
+env.hosts = ['66.70.184.249', '54.210.138.75']
 
 def deploy():
     """
