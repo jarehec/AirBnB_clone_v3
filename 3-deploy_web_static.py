682dcb4a95d255157b7818d1f1f0fc2babe185b8
Kimberly Wong <kjowong@gmail.com>
2017-08-22 05:21:05 +0000

    Adding imports

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,9 +4,12 @@
     on my web servers, using deploy function
 """
 from fabric.api import *
+from fabric.operations import run, put, sudo
+from datetime import datetime
 pack_web = __import__('1-pack_web_static')
 deploy_web = __import__('2-do_deploy_web_static')
 env.hosts = ['66.70.184.249', '54.210.138.75']
+
 
 def deploy():
     """
