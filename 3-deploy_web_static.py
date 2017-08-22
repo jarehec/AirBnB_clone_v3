f8536760b885db436b7494c74b08b2df89a1a442
Kimberly Wong <kjowong@gmail.com>
2017-08-22 08:58:02 +0000

    Removing imports

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,12 +7,53 @@ from fabric.api import *
 from fabric.operations import run, put, sudo, local
 from datetime import datetime
 import os
-from importlib import import_module
-pack_web = import_module('1-pack_web_static')
-deploy_web = import_module('2-do_deploy_web_static')
 
 env.hosts = ['66.70.184.249', '54.210.138.75']
 created_path = None
+
+
+def do_pack():
+    """
+        generates a .tgz archine from contents of web_static
+    """
+    time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    file_name = "versions/web_static_{}.tgz".format(time)
+    archive = ''
+    archive += local("mkdir -p ./versions")
+    archive += local("tar --create --verbose -z --file={} ./web_static"
+                     .format(file_name))
+    if archive is None:
+        return None
+    else:
+        return file_name
+
+
+def do_deploy(archive_path):
+    """
+        using fabric to distribute archive
+    """
+    if os.path.isfile(archive_path) is False:
+        return False
+    try:
+        archive = archive_path.split("/")[-1]
+        path = "/data/web_static/releases"
+        put("{}".format(archive_path), "/tmp/{}".format(archive))
+        folder = archive.split(".")
+        run("mkdir -p {}/{}/".format(path, folder[0]))
+        new_archive = '.'.join(folder)
+        run("tar -xzf /tmp/{} -C {}/{}/"
+            .format(new_archive, path, folder[0]))
+        run("rm /tmp/{}".format(archive))
+        run("mv {}/{}/web_static/* {}/{}/"
+            .format(path, folder[0], path, folder[0]))
+        run("rm -rf {}/{}/web_static".format(path, folder[0]))
+        run("rm -rf /data/web_static/current")
+        run("ln -sf {}/{} /data/web_static/current"
+            .format(path, folder[0]))
+        return True
+    except:
+        return False
+
 
 def deploy():
     """
@@ -20,7 +61,7 @@ def deploy():
     """
     global created_path
     if created_path is None:
-        created_path = pack_web.do_pack()
+        created_path = do_pack()
     if created_path is None:
         return False
-    return deploy_web.do_deploy(created_path)
+    return do_deploy(created_path)
