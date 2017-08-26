569b45c443394b12f8b428ebd1ba0471f2892e8e
Kimberly Wong <kjowong@gmail.com>
2017-08-26 17:35:08 +0000

    Initial commit

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Sript that starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def handle_teardown():
+    """
+        method to handle teardown
+    """
+    storage.close()
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    return render_template("7-states_list.html", states=storage.all('State'))
+
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
