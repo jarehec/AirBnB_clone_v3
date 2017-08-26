35333fff0d5048188b07a3ea32032f991807ed0e
Kimberly Wong <kjowong@gmail.com>
2017-08-26 12:01:28 +0000

    Style fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,12 +5,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbn():
     """
         function to return Hello HBNB!
     """
     return "Hello HBNB!"
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
