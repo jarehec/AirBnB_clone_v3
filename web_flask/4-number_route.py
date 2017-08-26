ff49d4f0f05a038af9b27ac2aee7fc988a3e00b6
Kimberly Wong <kjowong@gmail.com>
2017-08-26 14:13:36 +0000

    Initial commit

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+    Sript that starts a Flask web application
+ """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbn():
+    """
+        function to return Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+        function to return HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_var(text):
+    """
+        function to display text variable passed in
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def text_var_python(text="is cool"):
+    """
+        function to display text variable, with default "is cool"
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def var_num(n):
+        """
+             function to display a variable, but only if an int
+        """
+        return "{} is a number".format(n)
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
