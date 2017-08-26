e8abe63cce5d0dc0e831c071bdf384c8e8c835c6
Kimberly Wong <kjowong@gmail.com>
2017-08-26 19:10:13 +0000

    Changing jinja

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template
 from models import storage
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def handle_teardown():
     """
@@ -13,9 +14,11 @@ def handle_teardown():
     """
     storage.close()
 
+
 @app.route('/states_list', strict_slashes=False)
 def state_list():
-    return render_template("7-states_list.html", states=storage.all('State'))
+    states = storage.all('State').values()
+    return render_template("7-states_list.html", states=states)
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
