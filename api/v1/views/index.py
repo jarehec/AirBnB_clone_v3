e82836efc53d827de44f5457b275ff282b7ff040
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-11 22:29:20 -0700

    updated the circular import to be in the correct order & removed the line from pep8 style check

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,8 @@
 """
 Flask route that returns json status response
 """
-from flask import jsonify, Blueprint
-
-app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
+from flask import jsonify
+from api.v1.views import app_views
 
 
 @app_views.route('/status')
