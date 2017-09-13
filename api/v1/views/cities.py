4349e7aa8f8518788e07cc40fefd50cd2688dd93
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-13 14:18:57 -0700

    updated handling of errors, state_id & city_id in cities API for all GET PUT POST DELETE requests

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,24 +9,26 @@ from models import storage, CNC
 
 @app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'])
 @app_views.route('/cities/<city_id>', methods=['GET', 'DELETE', 'PUT'])
-def cities(city_id=None, state_id=None):
+def cities(state_id=None, city_id=None):
     """
         cities route to handle http method for requested states
     """
     all_cities = storage.all('City')
-    fetch_string = "{}.{}".format('City', city_id)
-    city_obj = all_cities.get(fetch_string)
+    fetch_city = "{}.{}".format('City', city_id)
+    city_obj = all_cities.get(fetch_city)
+    all_states = storage.all('State')
+    fetch_state = "{}.{}".format('State', state_id)
+    state_obj = all_states.get(fetch_state)
 
     if request.method == 'GET':
-        if city_id:
-            if city_obj:
-                return jsonify(city_obj.to_json())
-            else:
-                abort(404, 'Not found')
-        else:
-            all_cities = list(obj.to_json() for obj in all_cities.values()
-                              if obj.state_id == state_id)
-            return jsonify(all_cities)
+        if city_id and city_obj:
+            return jsonify(city_obj.to_json())
+        elif state_id and state_obj:
+            cities_per_state = [obj.to_json() for obj in
+                                all_cities.values()
+                                if obj.state_id == state_id]
+            return jsonify(cities_per_state)
+        abort(404, 'Not found')
 
     if request.method == 'DELETE':
         if city_obj:
@@ -36,24 +38,29 @@ def cities(city_id=None, state_id=None):
         abort(404, 'Not found')
 
     if request.method == 'POST':
-        if all_states.get(fetch_state) is None:
+        if state_id is None or state_obj is None:
             abort(404, 'Not found')
-            req_json = request.get_json()
+        req_json = request.get_json()
         if req_json is None:
             abort(400, 'Not a JSON')
         if req_json.get("name") is None:
             abort(400, 'Missing name')
         City = CNC.get("City")
+        req_json['state_id'] = state_id
         new_object = City(**req_json)
         new_object.save()
         return jsonify(new_object.to_json()), 201
 
     if request.method == 'PUT':
-        if city_id:
+        if city_id and city_obj:
             req_json = request.get_json()
             if city_obj is None:
                 abort(404, 'Not found')
             if req_json is None:
                 abort(400, 'Not a JSON')
+            IGNORE = ['id', 'state_id', 'created_at', 'updated_at']
+            attr_dict = {key: val for key, val in req_json.items()
+                         if key not in IGNORE}
             city_obj.bm_update(req_json)
             return jsonify(city_obj.to_json()), 200
+        abort(404, 'Not found')
