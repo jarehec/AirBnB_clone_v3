c80f811ac0b399596e5c3d786513c24ce12ac302
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-15 00:06:45 -0700

    beginning code for places_search API

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -70,3 +70,63 @@ def places_with_id(place_id=None):
             abort(400, 'Not a JSON')
         place_obj.bm_update(req_json)
         return jsonify(place_obj.to_json()), 200
+
+
+@app_views.route('/places_search', methods=['POST'])
+def places_search(city_id=None):
+    """
+        places route to handle http method for request to search places
+    """
+    all_places = storage.all('Place')
+
+    req_json = request.get_json()
+    if req_json is None:
+        return jsonify([
+            place.to_json() for place in all_places.values()
+        ])
+    states = req_json.get('states')
+    if states and len(states) > 0:
+        all_states = [
+            storage.get('State', s_id) for s_id in states
+        ]
+        all_cities = storage.all('City')
+        state_cities = [
+            city for city in all_cities.values() if city.state_id in all_states
+        ]
+    else:
+        state_cities = None
+    cities = req_json.get('cities')
+    if cities and len(cities) > 0:
+        city_objs = [
+            storage.get('City', c_id) for c_id in cities
+        ]
+    else:
+        city_objs = None
+    if state_cities and city_objs:
+        cities = state_cities
+        for city in city_objs:
+            if city not in state_cities:
+                cities.append(city)
+    elif state_cities:
+        cities = state_cities
+    elif city_objs:
+        cities = city_objs
+    else:
+        cities = None
+    amenities = req_json.get('amenities')
+    if amenities and cities:
+        return jsonify([
+        ])
+    if amenities is None and cities is None:
+        return jsonify([
+            place.to_json() for place in all_places.values()
+        ])
+    if cities and amenities is None:
+        return jsonify([
+            place.to_json() for place in all_places.values()
+            if place.city_id in cities
+        ])
+    if amenities and cities is None:
+        return jsonify([
+            place.to_json() for place in all_places.values()
+        ])
