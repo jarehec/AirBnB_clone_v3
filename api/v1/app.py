09ba6cc4489dace802c2d169e6de6177d24ca0f3
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-29 10:53:25 -0700

    updated error handling to function properly with gunicorn

--- api/v1/app.py
+++ api/v1/app.py
@@ -38,6 +38,28 @@ def teardown_db(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 errors, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
+
+
+@app.errorhandler(400)
+def handle_404(exception):
+    """
+    handles 400 errros, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
+
+
 @app.errorhandler(Exception)
 def global_error_handler(err):
     """
