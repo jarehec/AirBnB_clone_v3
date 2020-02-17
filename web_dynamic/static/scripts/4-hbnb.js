066e46d83e14aff12746a884a904ee20b63d7d51
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-02-17 07:24:34 +0000

    Fixed filtering functionality for places_amenity

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-    const amenities = {};
-    let places = [];
+	const amenities = {};
+	let places = [];
+	let newPlaces = []
 	$('input[type="checkbox"]').click(function () {
 		const amenityId = $(this).attr('data-id');
 		const amenityName = $(this).attr('data-name');
@@ -43,13 +44,12 @@ $(document).ready(function () {
 			contentType: "application/json",
 			data: JSON.stringify({}),
 			success: function (place) {
-				// places = []
+				$('article').remove()
+				places.length = 0
 				$.get( "http://0.0.0.0:5001/api/v1/users/", function(users) {
 					for (let i = 0; i < place.length; i++) {
 						for (let j = 0; j < users.length; j++) {
 							if (users[j].id === place[i].user_id) {
-								// first_name = users[j].first_name
-								// last_name = users[j].last_name
 								places.push(
 									`<article>
 				<div class="title">
@@ -91,44 +91,91 @@ $(document).ready(function () {
 							}
 						}
 					}
+					console.log(places)
 					$("section.places").append(places.join(''))
 				})
 			}
 		});
-    // console.log(places);
-    $('.container .filters button').click(function () {
-        $.ajax(
-                {
-                        url: 'http://0.0.0.0:5001/api/v1/places_search/',
-                        type: 'POST',
-                        dataType: 'json',
-                        contentType: "application/json",
-                        data: JSON.stringify({}),
-                        success: function (place) {
-			    let newPlace = {};
-			    // console.log(place);
-			    for (let l = 0; l < place.length; l++) {
-				// console.log(place[0].id)
-				// console.log(place[l].id)
-				// console.log("http://0.0.0.0:5001/api/v1/places/" + place[l].id + "/amenities");
-                                $.get( "http://0.0.0.0:5001/api/v1/places/" + place[l].id + "/amenities", function(place_amenities) {
-                                        // for (let i = 0; i < place.length; i++) {
-				    // console.log(place_amenities);
-                                                // for (let j = 0; j < place_amenities.length; j++) {
-						    for (let k = 0; k < Object.keys(amenities).length; k++) {
-							// console.log("INSIDE FOR LOOP!!!");
-place_amenities.forEach(dict => { if (Object.keys(amenities)[k] === dict.id) { newPlace[place[l].id]=place[l]; } }) // else { delete newPlace[place[l].id]; } } )
+	// console.log(places);
+	$('.container .filters button').click(function () {
+		$('article').remove()
+		newPlaces.length = 0
+		let newPlace = []
+		$.ajax(
+			{
+				url: 'http://0.0.0.0:5001/api/v1/places_search/',
+				type: 'POST',
+				dataType: 'json',
+				contentType: "application/json",
+				data: JSON.stringify({}),
+				success: function (place) {
+					for (let l = 0; l < place.length; l++) {
+						$.get( "http://0.0.0.0:5001/api/v1/places/" + place[l].id + "/amenities", function(place_amenities) {
+							amenities_count = 0
+							for (let k = 0; k < Object.keys(amenities).length; k++) {
+								for (let m = 0; m < place_amenities.length; m++) {
+									if (place_amenities[m].id === Object.keys(amenities)[k]) {
+										amenities_count += 1
+									}
+									// console.log("PLACE AMENTITIES KEYS === " + place_amenities[m].id)
+								}
+								// console.log("AMENITIES KEY=" + Object.keys(amenities)[k])
+							}
+							if (amenities_count === Object.keys(amenities).length) {
+								newPlace.push(place[l]);
+							}
+							// console.log(newPlace)
+						})
+					}
+					$.get( "http://0.0.0.0:5001/api/v1/users/", function(users) {
+						for (let i = 0; i < newPlace.length; i++) {
+							for (let j = 0; j < users.length; j++) {
+								if (users[j].id === newPlace[i].user_id) {
+									newPlaces.push(
+										`<article>
+				<div class="title">
+					<h2>
+					\#${ newPlace[i].name }
+					</h2>
+					<div class="price_by_night">
+					\$${ newPlace[i].price_by_night }
+					</div>
+					</div>
+					<div class="information">
+					<div class="max_guest">
+					<i class="fa fa-users fa-3x" aria-hidden="true"></i>
+					<br />
+					${ newPlace[i].max_guest } Guests
+					</div>
+					<div class="number_rooms">
+					<i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+					<br />
+					${ newPlace[i].number_rooms } Bedrooms
+					</div>
+					<div class="number_bathrooms">
+					<i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+					<br />
+					${ newPlace[i].number_bathrooms } Bathroom
+					</div>
+					</div>
+					<!-- **********************
+					USER
+					**********************  -->
+					<div class="user">
+					<strong>Owner: ${ users[j].first_name } ${ users[j].last_name }</strong>
+					</div>
+					<div class="description">
+					${ newPlace[i].description }
+					</div>
+					</article>
+							`);
+								}
+							}
 						}
-				})
-			    }
-			    console.log(newPlace);
-			    console.log(typeof(newPlace));
-			    for (let key in newPlace) {console.log(newPlace[key])};
-//			    Object.entries(newPlace).forEach( (k, v) => { console.log(k); console.log(v); } );
-//			    console.log(Object.values(newPlace));
-//			    console.log(newPlaceList);
-		}
-		}
-	);
-})
+						$("section.places").append(newPlaces.join(''))
+					})
+				}
+			}
+		);
+	})
 });
